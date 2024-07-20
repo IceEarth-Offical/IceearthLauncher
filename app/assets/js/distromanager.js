41b837d9924bcbbf622f8ac5e767163f0dec8268
Sejinjin <sejinjin1101@gmail.com>
2024-07-20 22:08:09 +0900

    Update package name and copyright information, localize app title and URLs, and support Iceearth server distribution.

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://iceearth-offical.github.io/FTP/iceearth_survive/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
